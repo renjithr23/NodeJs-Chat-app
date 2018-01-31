45de832ead7b48db5c3759b75700df3c872943bf
Renjith R <renjithr201097@gmail.com>
2018-01-31 18:23:20 +0530

    Great user from the admin

--- server/server.js
+++ server/server.js
@@ -20,30 +20,43 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('New user connected');
 
+  socket.emit("newMessage",{
+    from:"admin",
+    text:"Welcome to the chat app",
+    createdAt:new Date().getTime()
+  });
+
   // socket.emit("newEmail", {
   //   from:"mike@example.com",
   //   text:"Hey, this is created on ."
   // });
 
-  socket.emit("newEmail", {
-    from:"mike@example.com",
-    text:"Hey, this is created on ."
-  });
-
+  socket.broadcast.emit('newMessage',{
+    from:"admin",
+    text:"new user joined",
+    createdAt:new Date().getTime()
+  })
   //
   // socket.on('createEmail',(newEmail)=>{
   //   console.log('create email',newEmail);
   // })
 
 
-  socket.on('createMessage',(newMessage)=>{
-    console.log('New message received',newMessage);
+  socket.on('createMessage',(message)=>{
+    console.log('New message received',message);
     io.emit("newMessage",{
       from:newMessage.from,
       text:newMessage.text,
       createdAt:new Date().getTime()
     });
   })
+
+  //   socket.broadcast.emit('newMessage',{
+  //     from:message.from,
+  //     text:message.text,
+  //     createdAt:new Date().getTime()
+  //   })
+  // })
 
 
   socket.on('disconnect',()=>{
